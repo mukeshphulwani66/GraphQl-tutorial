1b63f549088c0b9ce46c4aaacff0037c0d0d3b60
mukesh phulwani <mukeshphulwani55@gmail.com>
2021-11-11 17:31:46 +0530

    lesson 2 done

---
+++ fakedb.js
@@ -0,0 +1,40 @@
+/*
+ collection/Table - Users 
+                    - doc1
+                    - doc2
+*/
+export const users = [
+    {
+        id:"23131",
+        firstName:"mukesh",
+        lastName:"kumar",
+        email:"mukesh@mukeshkumar.com",
+        password:"12345"
+    },
+    {
+        id:"4232",
+        firstName:"suresh",
+        lastName:"sharma",
+        email:"suresh@sureshsharma.com",
+        password:"12346"
+    },
+]
+
+/*
+ collection/Table - Quotes 
+                        - doc1
+                        - doc2
+                        - etc
+*/
+
+export const quotes = [
+    {
+        name:"I turn coffee into code",
+        by:"23131"
+    },
+    {
+        name:"If it works dont touch it",
+        by:"4232"
+    },
+
+] 
--- server.js
+++ server.js
@@ -2,17 +2,36 @@ import {ApolloServer,gql} from 'apollo-server'
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
 
 const typeDefs = gql`
-   type Query{
-       greet:String
-   }
+    type Query{
+        users:[User]
+        user(id:ID!): User
+        quotes:[Quote]
+        iquote(by:ID!):[Quote]
+    }
+    type User{
+        id:ID!
+        firstName:String
+        lastName:String
+        email:String
+        quotes:[Quote]
+    }
+    type Quote{
+        name:String
+        by:ID
+    }
 `;
 
 const resolvers = {
     Query:{
-        greet:()=>{
-            return "Hello world"
-        }
-    }
+        users:()=>users,
+        user:(_,{id})=>users.find(user=>user.id == id),
+        quotes:()=>quotes,
+        iquote:(_,{by})=> quotes.filter((quote)=>quote.by==by)
+        
+      },
+      User:{
+         quotes:(u)=> quotes.filter(quo=>quo.by == u.id)
+      },
 }
 
 
